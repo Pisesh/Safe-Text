56bf431b286f3d3dd45b202e89a706059bc5d54f
John <derij256@gmail.com>
2023-11-18 21:28:43 +0330

    create catch_values update

--- S@fe text.py
+++ S@fe text.py
@@ -6,7 +6,7 @@ import os
 
 # Create source matrix
 # 11 row and 9 coulmn
-# Decoder and encoder both of them use this matrix
+# Decryptor and encryptor both of them use this matrix
 
 Source_matrix = np.array([["a","b","c","d","e","f","g","h","i"],
                           ["j","k","l","m","n","o","p","q","r"],
@@ -19,3 +19,11 @@ Source_matrix = np.array([["a","b","c","d","e","f","g","h","i"],
                           ["&","*","(",")","/","\\","|","~","`"],
                           ["+","-","=","<",">","{","}","[","]"],
                           [":",";","\"","'",",","","","",""],])
+
+print("Welcome to the S@fe text encryptor\nThis program encrypt your message")
+
+# this function catch raw text from user and return it
+def catch_values ():
+    raw_text = input("Enter your message : \t")
+    return raw_text
+
