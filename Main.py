6bfe188e0d80b2c2d799f5f4781c3c9c9b8c0b7e
John <derij256@gmail.com>
2023-12-01 01:35:36 +0330

    develop and debuging account in main

--- Main.py
+++ Main.py
@@ -1,11 +1,27 @@
 # import modules
-import status as st
+from status import *
 import Cryptography as cg
+
+# create object from Account class
+userAccount = Account()
 
 def CreateAccount (name,password):
     
-    # create object from Account class
-    userAccount = st.Account()
-    return userAccount.CreateAccount(name,password)
-
+    # filter Null values
+    if name == "" or password == "":
+        return 1
     
+    else :
+        return userAccount.CreateAccount(name,password)
+
+def LoginAccount (name,password):
+
+    # filter Null values
+    if name == "" or password == "" :
+        return 1
+    
+    else :
+        return userAccount.LoginAccount(name,password)
+
+def CheckStatus ():
+    return userAccount.LoginStatus()
